0c0365f92e1b1dd0c1dff89bbfbf433e267c1179
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2019-05-22 17:57:22 +0200

    Home diferente en funcion del rol del usuario

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,27 +22,69 @@ export default {
             assists: []
         };
     },
+
     created(){
+
         firebase
         .firestore()
-        .collection("assists")
+        .collection("users")
+        .where("email", "==", firebase.auth().currentUser.email)
         .get()
         .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                
-                let dateIn = new Date(doc.data().In.seconds * 1000);
-                let dateOut = new Date(doc.data().Out.seconds * 1000);
+            if(querySnapshot.size > 0){
+                if(querySnapshot.docs[0].data().rol == "administrador"){
+                    console.log("Admin");
 
-                const data = {
-                    id: doc.data().id,
-                    user: doc.data().user,
-                    in: dateIn.getDay() + "/" + (dateIn.getMonth() + 1) + "/" + dateIn.getFullYear() + " " + dateIn.getHours() + ":" + dateIn.getMinutes() + ":" + dateIn.getSeconds(),
-                    out: dateOut.getDay() + "/" + (dateOut.getMonth() + 1) + "/" + dateOut.getFullYear() + " " + dateOut.getHours() + ":" + dateOut.getMinutes() + ":" + dateOut.getSeconds(),
-                };
-                
-                this.assists.push(data);
-            });
-        })
+                    firebase
+                    .firestore()
+                    .collection("assists")
+                    .get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(doc => {
+                            
+                            let dateIn = new Date(doc.data().In.seconds * 1000);
+                            let dateOut = new Date(doc.data().Out.seconds * 1000);
+
+                            const data = {
+                                id: doc.data().id,
+                                user: doc.data().user,
+                                in: dateIn.getDay() + "/" + (dateIn.getMonth() + 1) + "/" + dateIn.getFullYear() + " " + dateIn.getHours() + ":" + dateIn.getMinutes() + ":" + dateIn.getSeconds(),
+                                out: dateOut.getDay() + "/" + (dateOut.getMonth() + 1) + "/" + dateOut.getFullYear() + " " + dateOut.getHours() + ":" + dateOut.getMinutes() + ":" + dateOut.getSeconds(),
+                            };
+                            
+                            this.assists.push(data);
+                        });
+                    })
+                }else{
+                    console.log("No Admin");
+
+                    firebase
+                    .firestore()
+                    .collection("assists")
+                    .where("user", "==", firebase.auth().currentUser.email)
+                    .get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(doc => {
+                            
+                            let dateIn = new Date(doc.data().In.seconds * 1000);
+                            let dateOut = new Date(doc.data().Out.seconds * 1000);
+
+                            const data = {
+                                id: doc.data().id,
+                                user: doc.data().user,
+                                in: dateIn.getDay() + "/" + (dateIn.getMonth() + 1) + "/" + dateIn.getFullYear() + " " + dateIn.getHours() + ":" + dateIn.getMinutes() + ":" + dateIn.getSeconds(),
+                                out: dateOut.getDay() + "/" + (dateOut.getMonth() + 1) + "/" + dateOut.getFullYear() + " " + dateOut.getHours() + ":" + dateOut.getMinutes() + ":" + dateOut.getSeconds(),
+                            };
+                            
+                            this.assists.push(data);
+                        });
+                    })
+                }
+            }
+        },
+        err => {
+            alert(err.message);
+        });
     }
 };
 
